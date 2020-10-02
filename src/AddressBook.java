2f6d357dcbc665172518972adb06964e000474d0
Omar <omarhashmi@cmail.carleton.ca>
2020-10-02 07:45:02 -0400

    Added file

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -4,20 +4,19 @@ public class AddressBook {
     private List<BuddyInfo> thePals;
 
     public static void main(String[] args){
-        System.out.println("Fuck");
         BuddyInfo myPal=new BuddyInfo("Cahhrl","123 st","613");
         AddressBook myBook=new AddressBook();
         myBook.addBuddy(myPal);
         myBook.removeBuddy(myPal);
     }
 
-    AddressBook(){
-
+    public AddressBook(){
+        thePals=new ArrayList<>();
     }
     public void addBuddy(BuddyInfo newPal){
         thePals.add(newPal);
     }
     public void removeBuddy(BuddyInfo oldPal){
-        thePals.remove(thePals.indexOf(oldPal));
+        thePals.remove(oldPal);
     }
 }
